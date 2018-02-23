f9b54564274d7c806011b865ab668b6d6f26766a
himauli <himaulipatel2195@gmail.com>
2018-02-23 12:23:27 -0500

    Add files via upload

---
+++ Day4CustomTable/Day4CustomTable/MenuItem.swift
@@ -0,0 +1,19 @@
+//
+//  MenuItem.swift
+//  Day4CustomTable
+//
+//  Created by MacStudent on 2018-02-23.
+//  Copyright © 2018 Foram. All rights reserved.
+//
+
+import Foundation
+
+class MenuItems
+{
+    let names:[String] = ["Margherita Pizza","BBQ Chicken Pizza","Pepproni Pizza","Sausage Pizza","Seafood Pizza","Sausage Deep Dish","Meat Lover's Dish","Veggie Lover's Deep Dish","BBQ Chicken Deep Dish","Mushrrom Deep Dish"]
+    
+    let prices:[Double] = [7.95,11.49,8.45,8.45,12.75,10.65,12.35,10.00,16.60,11.25]
+    
+    let specials:[Bool] = [false,true,false,false,false,false,true,false,false,true]
+    
+}
