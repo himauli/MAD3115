8fb7512b25ea2d513cf145355dad1919e7b9d6e6
himauli <himaulipatel2195@gmail.com>
2018-03-02 11:05:55 -0500

    Add files via upload

---
+++ Day9Collection/Day9Collection/BooksCell.swift
@@ -0,0 +1,15 @@
+//
+//  BooksCell.swift
+//  Day9Collection
+//
+//  Created by MacStudent on 2018-03-02.
+//  Copyright © 2018 Foram. All rights reserved.
+//
+
+import UIKit
+
+class BooksCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imgBook: UIImageView!
+    @IBOutlet weak var lblBookTitle: UILabel!
+}
